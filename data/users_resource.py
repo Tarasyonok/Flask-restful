fe9dea1060bd35cc300a20066628d64d3ba15338
Kirill Tarasov <kir_tarasov@mail.ru>
2024-03-22 19:06:21 +0300

    Initial commit

---
+++ data/users_resource.py
@@ -0,0 +1,61 @@
+from flask_restful import reqparse, abort, Api, Resource
+import flask
+from data import db_session
+
+from data.users import User
+from data.jobs import Jobs
+from data.departments import Department
+
+
+class NewsResource(Resource):
+    def get(self, news_id):
+        abort_if_news_not_found(news_id)
+        session = db_session.create_session()
+        news = session.query(User).get(news_id)
+        return flask.jsonify({'news': news.to_dict(
+            only=('title', 'content', 'user_id', 'is_private'))})
+
+    def delete(self, news_id):
+        abort_if_news_not_found(news_id)
+        session = db_session.create_session()
+        news = session.query(User).get(news_id)
+        session.delete(news)
+        session.commit()
+        return flask.jsonify({'success': 'OK'})
+
+
+parser = reqparse.RequestParser()
+parser.add_argument('title', required=True)
+parser.add_argument('content', required=True)
+parser.add_argument('is_private', required=True, type=bool)
+parser.add_argument('is_published', required=True, type=bool)
+parser.add_argument('user_id', required=True, type=int)
+
+
+class NewsListResource(Resource):
+    def get(self):
+        session = db_session.create_session()
+        news = session.query(User).all()
+        return flask.jsonify({'news': [item.to_dict(
+            only=('title', 'content', 'user.name')) for item in news]})
+
+    def post(self):
+        args = parser.parse_args()
+        session = db_session.create_session()
+        news = News(
+            title=args['title'],
+            content=args['content'],
+            user_id=args['user_id'],
+            is_published=args['is_published'],
+            is_private=args['is_private']
+        )
+        session.add(news)
+        session.commit()
+        return flask.jsonify({'id': news.id})
+
+
+def abort_if_news_not_found(news_id):
+    session = db_session.create_session()
+    news = session.query(User).get(news_id)
+    if not news:
+        abort(404, message=f"News {news_id} not found")
